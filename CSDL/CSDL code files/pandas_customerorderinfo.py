00e2dc59978d5966aab15760ad33e3cd33ab9c5c
yen333vx-source <yen333vx@gmail.com>
2026-05-28 22:12:01 +0700

    Optimize SQL queries and DataFrame processing
    Refactor SQL queries to select specific columns and streamline DataFrame operations.

--- CSDL/CSDL code files/pandas_customerorderinfo.py
+++ CSDL/CSDL code files/pandas_customerorderinfo.py
@@ -2,14 +2,16 @@ import pymysql
 import pandas as pd
 
 conn = pymysql.connect(
-    host='localhost',
-    user='nhy',
-    password='Hoangyen9626!',
-    database='classicmodels'
+    host="localhost",
+    user="nhy",
+    password="Hoangyen9626!",
+    database="classicmodels"
 )
 
-customers = pd.read_sql("SELECT * FROM customers", conn)
-orders = pd.read_sql("SELECT * FROM orders", conn)
+customers = pd.read_sql("SELECT customerNumber, customerName FROM customers", conn)
+
+orders = pd.read_sql(
+    "SELECT orderNumber, customerNumber, orderDate, status FROM orders", conn)
 
 df = pd.merge(
     customers,
@@ -18,16 +20,7 @@ df = pd.merge(
     how="inner"
 )
 
-df = df[[
-    "customerNumber",
-    "customerName",
-    "orderNumber",
-    "orderDate",
-    "status"
-]]
-
-df = df.sort_values(by="orderNumber")
-df = df.head(5)
+df = df.sort_values(by="orderNumber").head(5)
 
 df.to_csv(
     "pandas_customer_orders.txt",
